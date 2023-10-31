1e36d7513bd67aec1b0a0f369ed14b186edde677
Nick Bernloehr <Nick Bernloehr@MY-AMD-DINGUS>
2023-10-31 10:02:23 -0400

    Add project files.

---
+++ Week10_real/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week10_real.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetMyName")]
+        public IActionResult GetMyName()
+        {
+            var myName = new { Name = "Nick Bernloehr" };
+            return Ok(myName);
+        }
+    }
+}
